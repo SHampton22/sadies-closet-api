1b3322b5674740dd39391b062fb42140b89535f3
Sarah Hampton <stoluba@gmail.com>
2023-03-01 13:28:18 -0500

    Update image url

--- data/clothes.js
+++ data/clothes.js
@@ -2,72 +2,72 @@ const clothes = [
   {
     "id": 1,
     "type": "top",
-    "img_url": ""
+    "img_url": "https://lh5.googleusercontent.com/RJi2XB-heJ9BWuFzFz_KYgyy2GGmMQP8P_2CLSJuVxkghUJNDR0FpYrjzFMtOrViN7E=w2400"
   },
   {
     "id": 2,
     "type": "top",
-    "img_url": ""
+    "img_url": "https://lh6.googleusercontent.com/nZ1nM2QTnNk2sbPY40gKUzu3nfuJm9k0Weqluq7gg3ebwJBXeTs9ykJgq6KJgUHNlpg=w2400"
   },
   {
     "id": 3,
     "type": "top",
-    "img_url": ""
+    "img_url": "https://lh6.googleusercontent.com/Xh10ZBQfQmnGk0fdLZnKxoMvT11L0TgrSA2byFHyzrtWWTCitcnYlDnQiZtjie6Rg2U=w2400"
   },
   {
     "id": 4,
     "type": "top",
-    "img_url": ""
+    "img_url": "https://lh4.googleusercontent.com/-HZDli2D0ynVD4v2GQwt8uhRujx5qnRWc3pt0MiDgQCdvtXS7q-u4SzlFvXAQb7R2rg=w2400"
   },
   {
     "id": 5,
     "type": "top",
-    "img_url": ""
+    "img_url": "https://lh3.googleusercontent.com/naWiHV21IZZGV0W6dccoIzsfVOdb1-_YKJIzTjbCHk6tkw4f7f_t1tZJmtDC0y9l7Nk=w2400"
   },
   {
     "id": 6,
     "type": "top",
-    "img_url": ""
+    "img_url": "https://lh6.googleusercontent.com/47b_bVpM3nlVc-KbWVFkTizbL1wT_Y-Gk8XL-MjzqsGi8c6Oow3zK6_NdHcEMozOntY=w2400"
   },
   {
     "id": 7,
     "type": "top",
-    "img_url": ""
+    "img_url": "https://lh6.googleusercontent.com/qkGOglmaL4YqbW31iQo6_lqUcQ4fuJHGeM95_N58eFlMJfuKrNsLifUW2ZXebtJC1dc=w2400"
   },
   {
     "id": 8,
     "type": "bottom",
-    "img_url": ""
+    "img_url": "https://lh4.googleusercontent.com/9lWM-UNJifU4kGqNGAt5borDBO0ikkx49FUHY7Z90QXZQHLdwOfiRmUo2A5wtNZ0loo=w2400"
   },
   {
     "id": 9,
     "type": "bottom",
-    "img_url": ""
+    "img_url": "https://lh4.googleusercontent.com/XSdbhNumZ3oINh58N8HzXFoS2qACbd-ch1GGsR9bW_iszQ3D4O670OOqyb4Yp7gQHEs=w2400"
   },
   {
     "id": 10,
     "type": "bottom",
-    "img_url": ""
+    "img_url": "https://lh6.googleusercontent.com/VGcd4iud-1whUMUJX5lcHLqLWmR29Itzfh-gH3kFEu4qUNFOFzWT4u1IpmcSoV0vUp0=w2400"
   },
   {
     "id": 11,
     "type": "bottom",
-    "img_url": ""
+    "img_url": "https://lh4.googleusercontent.com/EW9_EQQvING3RBeT_uQa_XCmzRucv_OHGXh9agRYo00FZjKhycJpYWz4KLI8nvM4Ils=w2400"
   },
   {
     "id": 12,
     "type": "bottom",
-    "img_url": ""
+    "img_url": "https://lh3.googleusercontent.com/KDtjBMpT2r5w3MegE0zotX1AbN6NdwOSktw894Vlwl01UdJ3YaODox3FtvfGtM4HyhQ=w2400"
   },
   {
     "id": 13,
     "type": "bottom",
-    "img_url": ""
+    "img_url": "https://lh4.googleusercontent.com/aWvqOwx1rH1Ngl0IuBoI0ZyzrQXXYrJaxzQaCxencDZQaQJNPS6WCq-PpSM7hxSB98o=w2400"
   },
   {
     "id": 14,
     "type": "bottom",
-    "img_url": ""
+    "img_url": "https://lh4.googleusercontent.com/hRU-quO1zSiWbfgxX-4tvg--fAl-JzWJn3w6Wh7EY8Yj5Tq_TUyQzmRlHEODOITkI3A=w2400"
   },
 ]
 export default clothes;
